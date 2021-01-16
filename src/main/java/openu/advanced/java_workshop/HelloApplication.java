947e0cfbe57f17a1457cbe5f58fff24fee69ed64
yohai <yohai7700@gmail.com>
2021-01-16 20:48:18 +0200

    Initial commit

---
+++ src/main/java/openu/advanced/java_workshop/HelloApplication.java
@@ -0,0 +1,9 @@
+package openu.advanced.java_workshop;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
