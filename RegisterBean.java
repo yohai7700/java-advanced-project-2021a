8e27a541bb1be1090b08e1fccc6ff8e0d091c9e5
OfekMarks <48588009+OfekMarks@users.noreply.github.com>
2021-03-19 19:42:02 +0200

    Update RegisterBean.java

--- RegisterBean.java
+++ RegisterBean.java
@@ -3,7 +3,6 @@ package openu.advanced.java_workshop;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
-@ManagedBean(name = "loginBean")
 @SessionScoped
 public class RegisterBean {
 
