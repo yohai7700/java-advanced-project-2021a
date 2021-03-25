264aa588d22a7b5ff6877e50950e56f97d907fd4
OfekMarks <ofekmarks@gmail.com>
2021-03-25 18:24:09 +0200

    bla

---
+++ src/main/java/openu/advanced/java_workshop/LoginBean.java
@@ -0,0 +1,27 @@
+package openu.advanced.java_workshop;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class LoginBean {
+
+    private String password, username;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
---
+++ src/main/java/openu/advanced/java_workshop/RegisterBean.java
@@ -0,0 +1,35 @@
+package openu.advanced.java_workshop;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class RegisterBean {
+
+    private String username, email, password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
