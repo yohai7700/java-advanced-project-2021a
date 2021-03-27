a3298189ac2a8b560341c553eb1c771202572a52
yohai <yohai7700@gmail.com>
2021-03-27 19:27:36 +0300

    fix logout

---
+++ src/main/java/openu/advanced/java_workshop/NavbarBean.java
@@ -0,0 +1,20 @@
+package openu.advanced.java_workshop;
+
+import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.IOException;
+import java.io.Serializable;
+
+@Named
+@SessionScoped
+public class NavbarBean implements Serializable {
+    @Inject
+    LoginBean loginBean;
+
+    public void handleLogout() throws IOException {
+        loginBean.logout();
+        FacesContext.getCurrentInstance().getExternalContext().redirect("/login.xhtml");
+    }
+}
