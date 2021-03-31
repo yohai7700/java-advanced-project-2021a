508ab88c4036acb10bbb7f17a89d496f3849031b
Dvir Dov <dvirdove@gmail.com>
2021-04-01 00:56:45 +0300

    coupon page update need an UI customization

--- src/main/java/openu/advanced/java_workshop/CouponsManagementBean.java
+++ src/main/java/openu/advanced/java_workshop/CouponsManagementBean.java
@@ -5,16 +5,52 @@ import openu.advanced.java_workshop.model.CouponsEntity;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
 import java.io.Serializable;
+import java.security.SecureRandom;
+import java.util.Base64;
 import java.util.List;
 
 @Named
 @RequestScoped
 public class CouponsManagementBean implements Serializable {
+    private final CouponsEntity newCoupon = new CouponsEntity();
+
+    public CouponsEntity getNewCoupon() {
+        return newCoupon;
+    }
+
     public List<CouponsEntity> getCoupons(){
         EntityManager entityManager = WorkshopDatabase.getEntityManagerFactory().createEntityManager();
         TypedQuery<CouponsEntity> findAllCoupons = entityManager.createNamedQuery("findAllCoupons", CouponsEntity.class);
         return findAllCoupons.getResultList();
+    }
+
+    private static String generateNewCouponCode() {
+        SecureRandom secureRandom = new SecureRandom(); //threadsafe
+        Base64.Encoder base64Encoder = Base64.getUrlEncoder(); //threadsafe
+
+        byte[] randomBytes = new byte[24];
+        secureRandom.nextBytes(randomBytes);
+        return base64Encoder.encodeToString(randomBytes);
+    }
+
+    private void addCouponToDB(String couponCode) {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("workshopPU");
+        EntityManager entityManager = factory.createEntityManager();
+
+        entityManager.getTransaction().begin();
+
+        newCoupon.setCode(couponCode);
+        System.out.println(newCoupon.getValue());
+        entityManager.persist(newCoupon);
+        entityManager.getTransaction().commit();
+    }
+
+    public void addCoupon() {
+        String coupon = generateNewCouponCode();
+        this.addCouponToDB(coupon);
     }
 }
