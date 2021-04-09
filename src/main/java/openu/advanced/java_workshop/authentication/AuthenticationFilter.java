94feb228af6c21510d021f855ef19f236646008d
yohai <yohai7700@gmail.com>
2021-04-09 18:12:07 +0300

    initial

--- src/main/java/openu/advanced/java_workshop/authentication/AuthenticationFilter.java
+++ src/main/java/openu/advanced/java_workshop/authentication/AuthenticationFilter.java
@@ -1,16 +1,10 @@
 package openu.advanced.java_workshop.authentication;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
+import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebFilter(filterName = "AuthFilter", urlPatterns = { "/secured/*" })
 public class AuthenticationFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) {
