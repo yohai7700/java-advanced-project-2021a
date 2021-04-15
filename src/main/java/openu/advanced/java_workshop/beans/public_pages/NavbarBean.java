6d4004eb8e591634b06a7e96224cc9e015be5ceb
yohai <yohai7700@gmail.com>
2021-04-15 23:47:47 +0300

    Add support for management of categories

--- src/main/java/openu/advanced/java_workshop/beans/public_pages/NavbarBean.java
+++ src/main/java/openu/advanced/java_workshop/beans/public_pages/NavbarBean.java
@@ -1,15 +1,20 @@
 package openu.advanced.java_workshop.beans.public_pages;
 
 import openu.advanced.java_workshop.SessionUtils;
+import openu.advanced.java_workshop.WorkshopDatabase;
 import openu.advanced.java_workshop.beans.LoginBean;
+import openu.advanced.java_workshop.model.CategoriesEntity;
 import openu.advanced.java_workshop.model.UsersEntity;
 
 import javax.enterprise.context.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * Bean class for the navbar.xhtml file. Manages the logics of the navigation bar
@@ -18,6 +23,8 @@ import java.io.Serializable;
 @Named
 @SessionScoped
 public class NavbarBean implements Serializable {
+    private static final int MAX_CATEGORIES = 5;
+
     @Inject
     LoginBean loginBean;
 
@@ -32,5 +39,15 @@ public class NavbarBean implements Serializable {
     public void handleLogout() throws IOException {
         loginBean.logout();
         FacesContext.getCurrentInstance().getExternalContext().redirect("/login.xhtml");
+    }
+
+    /**
+     * Get all of the categories in the database, max results is 5
+     * @return a list of all the categories (limited to max)
+     */
+    public List<CategoriesEntity> getCategories() {
+        EntityManager entityManager = WorkshopDatabase.getEntityManagerFactory().createEntityManager();
+        TypedQuery<CategoriesEntity> getCategories = entityManager.createNamedQuery("findAllCategories", CategoriesEntity.class);
+        return getCategories.setMaxResults(MAX_CATEGORIES).getResultList();
     }
 }
